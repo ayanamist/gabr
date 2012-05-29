196a5d9e779c3da79ab05892e6d618e8443dafa2
ayanamist <ayanamist@gmail.com>
2012-05-29 18:37:23 +0800

    initial version of oauth login

--- application/views/login.py
+++ application/views/login.py
@@ -1,8 +1,13 @@
+import logging
+import urlparse
+
 import flask
 
 from application import app
+from ..lib import oauth
+from ..lib import urlfetch
 
-@app.route("/login")
+@app.route("/login/")
 def login():
     data = {
         "title": "Login",
@@ -10,16 +15,25 @@ def login():
     return flask.render_template("login.html", **data)
 
 
-@app.route("/oauth", methods=["POST"])
+@app.route("/oauth/", methods=["POST"])
 def oauth_login():
     app_password = app.config["INVITE_CODE"]
     if (app_password and app_password == flask.request.form.get("invitecode")) or (not app_password):
-        pass
+        consumer = oauth.OAuthConsumer(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
+            callback=flask.url_for("oauth_callback"),
+            http_url="https://api.twitter.com/oauth/request_token")
+        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, None)
+        url = oauth_request.to_url()
+        resp = urlfetch.fetch(url).content
+        request_token = dict(urlparse.parse_qsl(resp))
+        oauth_token = request_token['oauth_token']
+        return flask.redirect("https://api.twitter.com/oauth/authorize?oauth_token=%s" % oauth_token)
     else:
-        flask.redirect(flask.url_for("login"))
+        return flask.redirect(flask.url_for("login"))
 
 
-@app.route("/oauth_callback")
+@app.route("/oauth_callback/")
 def oauth_callback():
     pass
 
