227acd8ffb100b0787c97ea849d155994004c135
ayanamist <ayanamist@gmail.com>
2012-05-29 15:41:15 +0800

    change login password to invite code avoiding messing up

--- application/views/login.py
+++ application/views/login.py
@@ -12,9 +12,14 @@ def login():
 
 @app.route("/oauth", methods=["POST"])
 def oauth_login():
-    pass
+    app_password = app.config["INVITE_CODE"]
+    if (app_password and app_password == flask.request.form.get("invitecode")) or (not app_password):
+        pass
+    else:
+        flask.redirect(flask.url_for("login"))
 
 
 @app.route("/oauth_callback")
 def oauth_callback():
     pass
+
