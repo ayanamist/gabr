a284f259dbe9172a5f11ceca3356273df7a2309c
ayanamist <ayanamist@gmail.com>
2012-05-29 18:39:30 +0800

    supplement the parenthesis

--- application/views/login.py
+++ application/views/login.py
@@ -15,22 +15,18 @@ def login():
     return flask.render_template("login.html", **data)
 
 
-@app.route("/oauth/", methods=["POST"])
+@app.route("/oauth/")
 def oauth_login():
-    app_password = app.config["INVITE_CODE"]
-    if (app_password and app_password == flask.request.form.get("invitecode")) or (not app_password):
-        consumer = oauth.OAuthConsumer(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
-        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
-            callback=flask.url_for("oauth_callback"),
-            http_url="https://api.twitter.com/oauth/request_token")
-        oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, None)
-        url = oauth_request.to_url()
-        resp = urlfetch.fetch(url).content
-        request_token = dict(urlparse.parse_qsl(resp))
-        oauth_token = request_token['oauth_token']
-        return flask.redirect("https://api.twitter.com/oauth/authorize?oauth_token=%s" % oauth_token)
-    else:
-        return flask.redirect(flask.url_for("login"))
+    consumer = oauth.OAuthConsumer(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
+        callback=flask.url_for("oauth_callback"),
+        http_url="https://api.twitter.com/oauth/request_token")
+    oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, None)
+    url = oauth_request.to_url()
+    resp = urlfetch.fetch(url).content
+    request_token = dict(urlparse.parse_qsl(resp))
+    oauth_token = request_token['oauth_token']
+    return flask.redirect("https://api.twitter.com/oauth/authorize?oauth_token=%s" % oauth_token)
 
 
 @app.route("/oauth_callback/")
