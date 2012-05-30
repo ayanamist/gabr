a8d9c89bb2f8474222d5830297c70504804fdc15
ayanamist <ayanamist@gmail.com>
2012-05-30 15:14:38 +0800

    change name for flask built in config

--- config-example.py
+++ config-example.py
@@ -1,4 +1,4 @@
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
-COOKIE_SECRET_KEY = '' # set this value as anything you like to enable cookie encryption.
+SECRET_KEY = '' # set this value as anything you like to enable cookie encryption.
 INVITE_CODE = '' # leave blank to serve as public service
