8f5f9bd9d5dd2aa91f6fc2894dc41bbe06df645a
ayanamist <ayanamist@gmail.com>
2012-05-30 15:19:45 +0800

    use permanent session

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,11 @@
+import datetime
+
 import flask
 
+flask.session.permanent = True
 app = flask.Flask("application")
 app.config.from_object("config")
+app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=365)
+app.config["SESSION_COOKIE_SECURE"] = True
 
 import views
