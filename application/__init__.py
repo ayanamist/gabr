a3922bb05d3058905d511d33200b56879df27cdc
ayanamist <ayanamist@gmail.com>
2012-05-30 17:04:48 +0800

    merge config.py into app.yaml

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,14 @@
 import datetime
+import os
 
 import flask
 
 app = flask.Flask("application")
-app.config.from_object("config")
 app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(days=365)
 app.config["SESSION_COOKIE_SECURE"] = True
+app.config["CONSUMER_KEY"] = os.environ["CONSUMER_KEY"]
+app.config["CONSUMER_SECRET"] = os.environ["CONSUMER_SECRET"]
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+app.config["INVITE_CODE"] = os.environ["INVITE_CODE"]
 
 import views
--- config-example.py
+++
@@ -1,4 +0,0 @@
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-SECRET_KEY = '' # set this value as anything you like to enable cookie encryption.
-INVITE_CODE = '' # leave blank to serve as public service
