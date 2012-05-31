639860fc911b35b5639a4c304efbf7388c1cc860
ayanamist <ayanamist@gmail.com>
2012-05-31 14:54:14 +0800

    use models use templated decorators

--- application/views/login.py
+++ application/views/login.py
@@ -1,26 +1,25 @@
-import logging
 import urlparse
 
 import flask
 
 from application import app
+from ..lib import decorators
 from ..lib import oauth
 from ..lib import urlfetch
+from ..models import twitter
 
 @app.route("/login/")
+@decorators.templated()
 def login():
-    data = {
-        "title": "Login",
-        }
-    return flask.render_template("login.html", **data)
+    return
 
 
 @app.route("/oauth/")
 def oauth_login():
     consumer = oauth.OAuthConsumer(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
-    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
-        callback="%s%s" % (flask.request.host_url, flask.url_for("oauth_callback")),
-        http_url="https://api.twitter.com/oauth/request_token")
+    callback_url = "%s%s" % (flask.request.host_url, flask.url_for("oauth_callback"))
+    oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, callback=callback_url,
+        http_url=twitter.REQUEST_TOKEN_URL)
     oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, None)
     url = oauth_request.to_url()
     try:
@@ -30,7 +29,7 @@ def oauth_login():
     request_token = dict(urlparse.parse_qsl(resp))
     oauth_token = request_token.get("oauth_token")
     if oauth_token:
-        return flask.redirect("https://api.twitter.com/oauth/authorize?oauth_token=%s" % oauth_token)
+        return flask.redirect("%s?oauth_token=%s" % (twitter.AUTHORIZE_URL, oauth_token))
     flask.flash("OAuth error, please try again.")
     return flask.redirect(flask.url_for("login"))
 
@@ -45,7 +44,7 @@ def oauth_callback():
         token = oauth.OAuthToken(oauth_token, None)
         token.set_verifier(oauth_verifier)
         oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer, token, verifier=oauth_verifier,
-            http_method="POST", http_url="https://api.twitter.com/oauth/access_token")
+            http_method="POST", http_url=twitter.ACCESS_TOKEN_URL)
         oauth_request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), consumer, token)
         url = oauth_request.to_url()
         try:
