8d4ae6b3ad9aa558cafded13bb5fe4ffec2c2f2e
ayanamist <ayanamist@gmail.com>
2012-09-03 15:37:20 +0800

    Fix instagram. Add img.ly and twitpic.

--- application/utils/preview.py
+++ application/utils/preview.py
@@ -1,7 +1,7 @@
 import re
 # All functions should return a tuple containing a thumbnail and a full image url.
 
-_instagram_regex = re.compile(r"(http(?:s)?://(?:instagram\.com|instagr\.am)?/p/[a-z0-9\-]+/)", re.I)
+_instagram_regex = re.compile(r"(http(?:s)?://(?:instagram\.com|instagr\.am)?/p/[a-z0-9\-_]+/)", re.I)
 
 def media_instagram(url):
     matched = _instagram_regex.match(url)
@@ -14,3 +14,17 @@ def media_sina(url):
     matched = _sina_regex.match(url)
     if matched:
         return ("%s%s%s" % (url[:matched.start(2)], t, url[matched.end(2):]) for t in ("small", "large"))
+
+_twitpic_regex = re.compile(r"(http(?:s)?://twitpic\.com/([0-9a-z]+)/)", re.I)
+
+def media_twitpic(url):
+    matched = _twitpic_regex.match(url)
+    if matched:
+        return ("%sshow/%s/%s" % (url[:matched.start(2)], t, url[matched.start(2):]) for t in ("thumb", "large"))
+
+_imgly_regex = re.compile(r"(http(?:s)?://img\.ly/([0-9a-z]+)/)", re.I)
+
+def media_imgly(url):
+    matched = _imgly_regex.match(url)
+    if matched:
+        return ("%sshow/%s/%s" % (url[:matched.start(2)], t, url[matched.start(2):]) for t in ("medium", "full"))
