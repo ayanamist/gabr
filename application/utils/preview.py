84475806cf7648f7a74f62518814888397365470
ayanamist <ayanamist@gmail.com>
2012-09-03 20:05:31 +0800

    Fix twitpic and img.ly with tailing slash.

--- application/utils/preview.py
+++ application/utils/preview.py
@@ -15,14 +15,14 @@ def media_sina(url):
     if matched:
         return ("%s%s%s" % (url[:matched.start(2)], t, url[matched.end(2):]) for t in ("small", "large"))
 
-_twitpic_regex = re.compile(r"(http(?:s)?://twitpic\.com/([0-9a-z]+)/)", re.I)
+_twitpic_regex = re.compile(r"(http(?:s)?://twitpic\.com/([0-9a-z]+))", re.I)
 
 def media_twitpic(url):
     matched = _twitpic_regex.match(url)
     if matched:
         return ("%sshow/%s/%s" % (url[:matched.start(2)], t, url[matched.start(2):]) for t in ("thumb", "large"))
 
-_imgly_regex = re.compile(r"(http(?:s)?://img\.ly/([0-9a-z]+)/)", re.I)
+_imgly_regex = re.compile(r"(http(?:s)?://img\.ly/([0-9a-z]+))", re.I)
 
 def media_imgly(url):
     matched = _imgly_regex.match(url)
