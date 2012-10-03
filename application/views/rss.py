bdbbeceaebd9846ed9d3e488579edde8a6d142fd
ayanamist <ayanamist@gmail.com>
2012-10-03 16:37:16 +0800

    Use reverse order.

--- application/views/rss.py
+++ application/views/rss.py
@@ -13,7 +13,7 @@ def home_rss():
     params = utils.parse_params()
     params["include_entities"] = 1
     data = timeline.timeline("Home", functools.partial(flask.g.api.getHomeTimeline, **params))
-    data["results"].sort(key=operator.itemgetter("id")) # Google Reader will not sort it themselves.
+    data["results"].sort(key=operator.itemgetter("id"), reverse=True)
     data["now"] = email.utils.formatdate()
     resp = flask.make_response(flask.render_template("rss.xml", **data))
     resp.headers["Content-Type"] = "application/rss+xml"
