6437f173b572ef23f117bd32df96f13588b4d97f
ayanamist <ayanamist@gmail.com>
2012-10-19 17:26:15 +0800

    Increase RSS items to 200. Add memcache.

--- application/views/rss.py
+++ application/views/rss.py
@@ -1,7 +1,10 @@
 import base64
 import email.utils
 import functools
+import json
 import operator
+
+from google.appengine.api import memcache
 
 import flask
 import twython
@@ -13,18 +16,27 @@ from application import app
 
 @app.route("/rss/<sid>")
 def home_rss(sid):
+    sid = str(sid)
     try:
-        sid = crypto.decrypt(base64.urlsafe_b64decode(str(sid)), app.config["SECRET_KEY"])
+        sid = crypto.decrypt(base64.urlsafe_b64decode(sid), app.config["SECRET_KEY"])
         oauth_token, oauth_token_secret = sid.split(":", 1)
     except (ValueError, TypeError):
         return "Invalid sid."
     flask.g.api = twython.Twython(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"],
         oauth_token, oauth_token_secret)
-
     params = utils.parse_params()
-    params["include_entities"] = 1
-    data = timeline.timeline("Home", functools.partial(flask.g.api.getHomeTimeline, **params))
-    data["results"].sort(key=operator.itemgetter("id"), reverse=True)
+    cached = memcache.get(sid + str(params))
+    if cached:
+        data = {
+            "title": "Home",
+            "results": json.loads(cached)
+        }
+    else:
+        params["include_entities"] = 1
+        params["count"] = 200
+        data = timeline.timeline("Home", functools.partial(flask.g.api.getHomeTimeline, **params))
+        data["results"].sort(key=operator.itemgetter("id"), reverse=True)
+        memcache.set(sid + str(params), json.dumps(data["results"]), time=60)
     data["now"] = email.utils.formatdate()
     resp = flask.make_response(flask.render_template("rss.xml", **data))
     resp.headers["Content-Type"] = "application/rss+xml"
