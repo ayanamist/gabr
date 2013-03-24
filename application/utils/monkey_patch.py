3b1b61bf395e115d7ee2234ad9bba4dbdcb203cf
ayanamist <ayanamist@gmail.com>
2013-03-24 21:17:47 +0800

    More elegant logging patch way.

--- application/utils/monkey_patch.py
+++ application/utils/monkey_patch.py
@@ -12,14 +12,18 @@ from . import abs_url_for
 
 
 def patch_logging():
-    def logger_debug(*args, **kwargs):
-        caller_frame = inspect.currentframe().f_back
-        caller_name = caller_frame.f_globals["__package__"]
-        logger = logging.getLogger(caller_name)
-        logger.debug(*args, **kwargs)
+    def logger_wrap(func):
+        def logger_log(*args, **kwargs):
+            caller_frame = inspect.currentframe().f_back
+            caller_name = caller_frame.f_globals["__package__"]
+            logger = logging.getLogger(caller_name)
+            getattr(logger, func.__name__)(*args, **kwargs)
+
+        return logger_log
 
     # Replace all direct function callings to logger methods.
-    logging.debug = logger_debug
+    for func_name in ("critical", "error", "exception", "warn", "warning", "info", "debug", "log"):
+        setattr(logging, func_name, logger_wrap(getattr(logging, func_name)))
 
     # Mute noisy logging.
     logging.getLogger("requests").setLevel(logging.ERROR)
